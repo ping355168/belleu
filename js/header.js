41aa10fe639f33a9bde460652578c8cd36853461
ping355168 <ping355168@gmail.com>
2024-07-27 00:44:44 +0800

    rwd

--- js/header.js
+++ js/header.js
@@ -5,3 +5,21 @@ $(window).on('scroll', function() {
         $('header').removeClass('scrolled');
     }
 });
+
+
+$(document).ready(function() {
+    $('#barlogo').click(function() {
+        $('.barmenu').css('display', 'block').animate({
+            left: '-800px'
+        }, 200); 
+    });
+
+    $('#delete').click(function() {
+        $('.barmenu').animate({
+            left: '100px'
+        }, 200); 
+    });
+});
+
+
+
